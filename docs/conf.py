7e7237135ceeae32977b7255944e4f46531f04b2
Chengxu Bian <piensengxv@gmail.com>
2025-05-11 10:44:40 -0400

    add conf.py

---
+++ docs/conf.py
@@ -0,0 +1,42 @@
+# Configuration file for the Sphinx documentation builder.
+
+import os
+import sys
+
+# -- Path setup --------------------------------------------------------------
+
+# If your module is in the parent directory, add it to sys.path
+sys.path.insert(0, os.path.abspath(".."))
+
+# -- Project information -----------------------------------------------------
+
+project = "rdf4j-python"
+copyright = "2025, Chengxu Bian"
+author = "Chengxu Bian"
+release = "0.1.1a"
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",  # for Google/NumPy-style docstrings
+    "sphinx.ext.viewcode",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = "furo"  # Or 'sphinx_rtd_theme', 'alabaster', etc.
+html_static_path = ["_static"]
+html_context = {
+    "display_github": True,
+    "github_user": "odysa",
+    "github_repo": "rdf4j-python",
+    "github_version": "develop",
+    "conf_py_path": "/docs/",
+}
+
+
+exclude_patterns = ["dist", ".DS_Store", ".venv", ".pytest_cache"]
