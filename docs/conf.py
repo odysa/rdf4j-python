4f9770112e754188b1feac9568fcd2b1d89fe64e
Chengxu Bian <piensengxv@gmail.com>
2025-05-12 23:39:23 -0400

    fix: remove doc config (#15)

---
+++ docs/conf.py
@@ -0,0 +1,49 @@
+# Configuration file for the Sphinx documentation builder.
+
+import os
+import sys
+
+# -- Path setup --------------------------------------------------------------
+
+# If your module is in the parent directory, add it to sys.path
+sys.path.insert(0, os.path.abspath(".."))
+
+# -- Project information -----------------------------------------------------
+
+project = "rdf4j-python"
+copyright = "2025, Chengxu Bian"
+author = "Chengxu Bian"
+release = "0.1.1a"
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",  # for Google/NumPy-style docstrings
+    "sphinx.ext.viewcode",
+    "myst_parser",
+]
+
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".txt": "markdown",
+    ".md": "markdown",
+}
+
+templates_path = ["_templates"]
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = "alabaster"  # Or 'sphinx_rtd_theme', 'alabaster', etc.
+html_context = {
+    "display_github": True,
+    "github_user": "odysa",
+    "github_repo": "rdf4j-python",
+    "github_version": "develop",
+    "conf_py_path": "/docs/",
+}
+
+
+exclude_patterns = ["dist", ".DS_Store", ".venv", ".pytest_cache"]
+
+master_doc = "index"
---
+++ docs/index.md
@@ -0,0 +1,13 @@
+# rdf4j-python
+
+**A Pythonic interface to the powerful Java-based [Eclipse RDF4J](https://rdf4j.org/) framework.**
+
+> ⚠️ **Experimental:** This project is under active development. Interfaces may change. Use with caution in production—and feel free to contribute!
+
+✅ **Supports both asynchronous (`async/await`) and synchronous usage.**
+
+---
+
+## Overview
+
+`rdf4j-python` bridges Python applications with the [Eclipse RDF4J](https://rdf4j.org/) framework, enabling seamless interaction with RDF4J repositories. It allows Python developers to harness RDF4J's robust RDF data management and SPARQL querying capabilities without leaving the Python
