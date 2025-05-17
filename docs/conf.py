419a1e53acfa3f7f700b90b9e6d3d99c94f21b06
Chengxu Bian <cbian564@gmail.com>
2025-05-17 01:23:34 -0400

    fix

--- docs/conf.py
+++ docs/conf.py
@@ -9,13 +9,13 @@ sys.path.insert(0, os.path.abspath(".."))
 
 import datetime
 
-from rdf4j_python import __author__, __version__
+from rdf4j_python import __authors__, __version__
 
 # -- Project information -----------------------------------------------------
 
 project = "rdf4j-python"
-copyright = f"{datetime.date.today().year}, {__author__}"
-author = __author__
+copyright = f"{datetime.date.today().year}, {__authors__[0]['name']}"
+author = __authors__[0]["name"]
 version = __version__
 release = __version__
 
