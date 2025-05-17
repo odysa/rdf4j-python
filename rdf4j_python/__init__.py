1b61330024cee2ba87147bdc47c5cc1393dbaf46
Chengxu Bian <cbian564@gmail.com>
2025-05-17 02:19:05 -0400

    add docs

--- rdf4j_python/__init__.py
+++ rdf4j_python/__init__.py
@@ -1,9 +1,8 @@
-from ._client import AsyncApiClient, SyncApiClient
 from ._driver import AsyncNamedGraph, AsyncRdf4j, AsyncRdf4JRepository
+from .model import *  # noqa: F403
+from .utils import *  # noqa: F403
 
 __all__ = [
-    "AsyncApiClient",
-    "SyncApiClient",
     "AsyncRdf4j",
     "AsyncRdf4JRepository",
     "AsyncNamedGraph",
