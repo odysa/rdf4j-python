dc8289db159e3461edfca9c15ccff3e3227afccd
Chengxu Bian <cbian564@gmail.com>
2025-05-17 02:43:09 -0400

    test

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ templates_path = ["_templates"]
 
 # -- Options for HTML output -------------------------------------------------
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 html_context = {
     "display_github": True,
     "github_user": "odysa",
